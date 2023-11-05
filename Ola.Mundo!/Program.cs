c453977d00596c0ce5f08e41cf967bba42283dbc
sivaldo vieira <vieiralmeida.com@hotmail.com>
2023-11-05 20:59:07 -0300

    Modificando conteudo

--- Ola.Mundo!/Program.cs
+++ Ola.Mundo!/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.IO.Compression;
+
+int x = 10;
+int y = 20;
+int z = x + y;
+
+
+
+
+
+
+Console.WriteLine(z);
